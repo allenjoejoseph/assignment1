5c2e4834926b81c604658f8ca5b2e4a5c4b7320f
Allen Joe Joseph <62803056+magicfiesta@users.noreply.github.com>
2021-11-11 20:30:12 +0530

    Update form.js

--- form.js
+++ form.js
@@ -19,9 +19,11 @@ function validate(){
     else{
         return true
     }
-    
-   var regEx = ^\+{0,2}([\-\. ])?(\(?\d{0,3}\))?([\-\. ])?\(?\d{0,3}\)?([\-\. ])?\d{3}([\-\. ])?\d{4};
-   if(num.value.match(regEx))
+}
+ 
+function phoneValidate(num){
+   var s = ^\+{0,2}([\-\. ])?(\(?\d{0,3}\))?([\-\. ])?\(?\d{0,3}\)?([\-\. ])?\d{3}([\-\. ])?\d{4};
+   if(num.value.match(s))
      {
       return true;
      }
